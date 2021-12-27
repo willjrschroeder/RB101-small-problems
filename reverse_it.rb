1caa22bff65be7aab22008878baf673a7a8b3b6b
Will Schroeder <willjrschroeder@gmail.com>
2021-12-27 08:52:53 -0600

    completed part 2 of the reverse_it excercise

--- reverse_it.rb
+++ reverse_it.rb
@@ -1,4 +1,3 @@
-
 def string_reverser(original_string)
   original_array = original_string.split
   reversed_string = ""
@@ -12,3 +11,34 @@ end
 puts string_reverser("Hello World")
 puts string_reverser("Hello my name is Will")
 puts string_reverser("       ") == ""
+
+def word_reverser(string)
+  output_str = ""
+  words_array = string.split
+  return words_array[0].reverse if words_array.length == 1 && words_array[0].length >= 5
+  return words_array[0] if words_array.length == 1
+
+  words_array.each do |word|
+    if word.length >= 5
+      output_str << word.reverse + " "
+    else
+      output_str << word + " "
+    end
+  end
+  output_str
+end
+
+# Refactored word_reverser method
+
+def word_reverser_mk2(string)
+  words = []
+
+  string.split.each do |word|
+    word.reverse! if word.length >= 5
+    words << word
+  end
+  words.join(" ")
+end
+
+puts word_reverser("TESTER")
+puts word_reverser("Hello my name is William")
