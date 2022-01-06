d9e56b5d2c65065446ec9f1612433f7e62c9dd5c
Will Schroeder <willjrschroeder@gmail.com>
2022-01-06 12:24:51 -0600

    added implementation

--- odd_lists.rb
+++ odd_lists.rb
@@ -13,8 +13,20 @@ def oddities(array)
   output_array
 end
 
-p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
-p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
-p oddities(['abc', 'def']) == ['abc']
-p oddities([123]) == [123]
-p oddities([]) == []
+def oddities_2(array)
+  counter = 0
+  output_array = []
+
+  loop do
+    break unless counter < array.size
+    output_array.push(array[counter])
+    counter += 2
+  end
+  output_array
+end
+
+p oddities_2([2, 3, 4, 5, 6]) == [2, 4, 6]
+p oddities_2([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+p oddities_2(['abc', 'def']) == ['abc']
+p oddities_2([123]) == [123]
+p oddities_2([]) == []
